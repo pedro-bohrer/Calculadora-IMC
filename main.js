fbc45f40d9a3f8dbe4900fdb395c2ac415cc261f
Pedro H. Bohrer <129559893+pedro-bohrer@users.noreply.github.com>
2023-12-28 16:19:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function skip() {
-    document.write("<br>");           
+    document.write("<br><br>");           
 }
 function txt(texto) {
     document.write(texto);
@@ -9,19 +9,18 @@ function calculo(peso, altura) {
     return imc = peso / (altura * altura);
 }
 
-var nome1 = prompt("informe o primeiro Nome: ");
-var peso1 = prompt("informe o primeiro peso: ");
-var alt1 = prompt("informe a primeira Altura: ");
+var nome = prompt("informe o seu Nome: ");
+var peso = prompt("informe o seu peso: ");
+var alt = prompt("informe a sua Altura: ");
 
-var nome2 = prompt("informe o segundo Nome: ");
-var peso2 = prompt("informe o segundo peso: ");
-var alt2 = prompt("informe a segundo Altura: ");
+var imc = Math.round(calculo(peso, alt));
 
-var imc1 = Math.round(calculo(peso1, alt1));
-var imc2 = Math.round(calculo(peso2, alt2));
-var imcSoma = imc1 + imc2;
+txt("<h3>O imc de " + nome + " é de " + imc);
+if (imc < 18.5) {
+   txt("Você está abaixo do recomendado") 
+} else if (imc > 35) {
+    txt("Você está acima do recomendado")  
+} else if (imc >= 18.5 && imc <= 35) {
+    txt("Seu imc está excelente!")  
+}
 
-txt("O imc de " + nome1 + " é: " + imc1);
-txt("O imc de " + nome2 + " é: " + imc2);
-skip();
-txt("A soma dos dois imc's é igual a " + imcSoma + "<br>E sua média é igual a: " + (imcSoma/2));
